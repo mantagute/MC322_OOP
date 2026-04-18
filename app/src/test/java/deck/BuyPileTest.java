c746bf0c7b7751b2faff1f19ea9badf57a9c22ef
Mantagute <joaogabrieliuzviakmantagute@gmail.com>
2026-04-18 11:10:53 -0300

    feat: BuyPile and Hand Tests

--- app/src/test/java/deck/BuyPileTest.java
+++ app/src/test/java/deck/BuyPileTest.java
@@ -1,8 +1,31 @@
 package deck;
 
+import cards.DamageCard;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class BuyPileTest {
-    
+    @Test
+    public void BuyPileisRenewed() {
+        BuyPile buypile = new BuyPile();
+        DiscardPile discardPile = new DiscardPile();
+        DamageCard card1 = new DamageCard("carta1", 3, 10, null, false);
+        DamageCard card2 = new DamageCard("card2", 3, 10, null, false);
+        buypile.push(card1);
+        buypile.push(card2);
+        buypile.drawCard(discardPile);
+        buypile.drawCard(discardPile);
+        discardPile.push(card1);
+        discardPile.push(card2);
+        buypile.drawCard(discardPile);
+        assertTrue(buypile.getSize() != 0);
+    }
+
+    @Test 
+    public void drawCardFromEmptyBuyPileandEmptyDiscardPile() {
+        BuyPile buypile = new BuyPile();
+        DiscardPile discardPile = new DiscardPile();
+        assertNull(buypile.drawCard(discardPile));
+    }
+
 }
